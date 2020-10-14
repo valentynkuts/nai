1c5f78023dc11c38598b0fd31ccd1560f84de527
valentyn <valik.kuts@gmail.com>
2020-10-14 18:10:27 +0200

    add mirror frame

--- lab1/v1.cpp
+++ lab1/v1.cpp
@@ -3,27 +3,44 @@
 #include <iostream>
 
 int main( int argc, char** argv ) {
+
     bool capturing = true;
     // Question for you
     // cv::VideoCapture cap( "szukaj_zielonego.webm" );
-    cv::VideoCapture cap(0);
+    cv::VideoCapture cap(0); //open camera
 
     if ( !cap.isOpened() ) {
         std::cerr << "error opening frames source" << std::endl;
         return -1;
     }
+
     std::cout << "Video size: " << cap.get( cv::CAP_PROP_FRAME_WIDTH ) 
     << "x" << cap.get( cv::CAP_PROP_FRAME_HEIGHT ) << std::endl;
+
     do {
-        cv::Mat frame;
+        cv::Mat frame;// declaire frame
+        cv::Mat frameMirror;
+
         if ( cap.read( frame ) ) {
-            cv::imshow( "Not-yet smart windown", frame );
+            //flip(source, destination, flipcode)
+            //flipcode:
+            //1 - flip around y-axis
+            //0 - flip around x-axis
+            //-1 -flip both axes
+            cv::flip(frame, frameMirror, 1);
+            cv::imshow( "Not-yet smart windown", frame );// imshow(window name, frame)
+            cv::imshow("Odbicie lustrzane", frameMirror );
+
         } else {
+            
             // stream finished
             capturing = false;
         }
         //czekaj na klawisz, sprawdz czy to jest 'esc'
-        if( (cv::waitKey( 1000.0/60.0 )&0x0ff) == 27 ) capturing = false;
+        if( (cv::waitKey( 1000.0/60.0 )&0x0ff) == 27 )
+            capturing = false;
+
     } while( capturing );
+
     return 0;
 } 
